18574ea465eb66a710635562081500a549b5a846
Dr. Taylor <taylor@mst.ed>
2019-01-23 21:55:20 -0600

    Initial commit

---
+++ MyVector.hpp
@@ -0,0 +1,22 @@
+/* Define all your MyVector-related functions here.
+ * You do not need to include the h file.
+ * It included this file at the bottom.
+ */
+
+// We're giving you this one for free
+// and as a guide for the syntax.
+template <typename T>
+MyVector<T>::~MyVector()
+{
+    delete[] m_data;
+}
+
+
+// Another example: remember when writing an implementation in hpp,
+// return type first, then scope just before the function name.
+template <typename T>
+T & MyVector<T>::at(int index)
+{
+    //Define the function here.
+}
+
